09ce5b85180e28a872c4c115a7e1d4aba4240821
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-09 20:02:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+string = array.join
+return string
+elsif array.size == 2
+  string = array.join(" and ")
+  return string
+elsif array.size == 3
+  string = array[0..1].join(", ") + ", and " + array[2]
+  return string
+elsif array.size > 3
+  string = array[0..-2].join(", ") + ", and " + array[-1]
+  return string
+  end
 end
