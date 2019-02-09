ccda5600f9ed608b2126ad6aa4a6ea9d317b5856
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-09 19:42:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ elsif array.size == 2
 elsif array.size == 3
   array.insert(2,"and")
   #array.insert(1,<< ",")
-  string = array[0..1].join(",")
+  string = array[0..1].join(", ")
   return string
   end
 end
