9aee43a91beee7932ed8550657e611ac9b695838
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-09 19:59:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ elsif array.size == 3
   string = array[0..1].join(", ") + ", and " + array[2]
   return string
 elsif array.size > 3
-  string = array[0..1].join(", ") + ", and " + array[-1]
+  string = array[0..-2].join(", ") + ", and " + array[-1]
   return string
   end
 end
